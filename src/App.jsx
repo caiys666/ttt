5e4dc7f92b1cc9c6cdfa03f89cc959e21daa200a
DESKTOP-HVAQES8\Administrator <1255651384@qq.com>
2021-07-13 16:41:41 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Test from './components/test/test';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+                <Test />
+            </div>
+        );
+    }
+}
